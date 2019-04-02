8f4a3b63e9f0976332cde3a57be5e1b4c1835d69
Robert <rmolinamir@gmail.com>
2019-04-02 00:22:47 -0400

    New color generator for SVG underlines

--- src/lib/Underlines/index.js
+++ src/lib/Underlines/index.js
@@ -78,15 +78,16 @@ Underline.propTypes = {
 }
 
 /**
- * Light colors generator.
+ * Pastel palette colors generator.
+ * Based on:
+ * **`[Random color generator](https://stackoverflow.com/questions/1484506/random-color-generator)`**
  */
-const getRandomColor = () => {
-  const letters = '890ABCDEF'.split('')
-  let color = '#'
-  for (let i = 0; i < 3; i++) {
-    color += letters[Math.floor(Math.random() * letters.length)]
-  }
-  return color
+export const getRandomColor = (pastelIndex = 2.5) => {
+  // Six levels of pastelIndex from 0 to 5, 0 being the darkest.
+  const rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256]
+  const mix = [pastelIndex * 51, pastelIndex * 51, pastelIndex * 51] // 51 => 255/5
+  const mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map((x) => { return Math.round(x / 2.0) })
+  return 'rgb(' + mixedrgb.join(',') + ')'
 }
 
 export default React.memo(Underline)
